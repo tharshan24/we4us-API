04c95234520f82df32c707cd89dedd02ad802ff4
ThishanJude <thishan1997@gmail.com>
2021-07-22 00:06:11 +0530

    Public_Profile_Update

--- app/controllers/public_controller.js
+++ app/controllers/public_controller.js
@@ -2,7 +2,7 @@ const publicService = require('../services/public_service');
 
 // registration of public
 function viewProfile(req,res){
-    console.log(req.params);
+    //console.log(req.params);
     publicService.viewProfile(req.params,function(err,results){
         if(err){
             res.json({status_code:1,message:'Cannot get profile',error:err.message});
@@ -20,6 +20,27 @@ function viewProfile(req,res){
     });
 }
 
+// updateProfile of public
+function updateProfile (req,res){
+   // console.log(req);
+    publicService.updateProfile(req.body,function(err,results){
+        if(err){
+            res.json({status_code:1,message:'Cannot Update profile',error:err.message});
+        }
+        else{
+            // console.log(results);
+            res.json({
+                status_code:0,
+                message:'success',
+                result:results,
+                authData: req.authData,
+                token: req.token
+            });
+        }
+    });
+}
+
 module.exports = {
-    viewProfile:viewProfile
+    viewProfile:viewProfile,
+    updateProfile:updateProfile
 }
