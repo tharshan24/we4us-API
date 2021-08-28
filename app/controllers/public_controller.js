ca66961c041f8b83b99a4e630fc9ad65bf1894b1
ThishanJude <thishan1997@gmail.com>
2021-08-28 14:38:45 +0530

    removed_createavailability

--- app/controllers/public_controller.js
+++ app/controllers/public_controller.js
@@ -60,29 +60,9 @@ function registerDriver (req, res){
     });
 }
 
-//createAvailability of public
-function createAvailability (req, res){
-    //console.log(req.body)
-    publicService.createAvailability(req, function(err, results){
-        if(err){
-            res,json({status_code:1, message: 'Cannot create Availability', error: err.message});
-        }
-        else{
-            console.log(results)
-            res.json({
-                status_code: 0,
-                message: 'Availability creation Success',
-                result: results,
-                authData: req.body.authData,
-                token: req.token
-            });
-        }
-    });
-}
 
 module.exports = {
     viewProfile:viewProfile,
     updateProfile:updateProfile,
-    registerDriver:registerDriver,
-    createAvailability:createAvailability
+    registerDriver:registerDriver
 }
