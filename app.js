9f0c59b1e8e13fbd5338954daddcbba9cd087b40
Priyatharshan <tharshan24@gmail.com>
2021-09-14 12:00:14 +0530

    deploy fix 2

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const systemRouter = require('./app/routes/system_routes');
 const availabilityRouter = require('./app/routes/availability_routes');
 const mongoose = require('mongoose');
 const app = express();
-const port = 8000;
+// const port = 8000;
+const port = process.env.PORT || 80;
 
 // console.log(main);
 
@@ -43,5 +44,9 @@ app.use('/system', systemRouter);
 app.use('/availability', availabilityRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`We4uS app listening at http://localhost:${port}`);
 });
+
+// app.listen('https://we4us.herokuapp.com/', () => {
+//   console.log(`We4uS app listening at https://we4us.herokuapp.com/`);
+// });
